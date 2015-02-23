9c31d501ff83981ef62060a39c48b528ce689471
PowerRabbit <ivan.vlassov@gmail.com>
2015-02-23 14:39:25 +0300

    Formated.

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,18 +1,38 @@
-javascript : var xhr = new XMLHttpRequest();
-var params = {url: window.top.location};
-xhr.withCredentials = true;
-xhr.open("POST", "http://127.0.0.1:8090/set", true);
-xhr.setRequestHeader("Accept", "application/json");
-xhr.setRequestHeader("Content-type","application/json");
-xhr.onreadystatechange = function () {
-	if (xhr.readyState == 4 && xhr.status == 200) {
-		var resp = JSON.parse(xhr.responseText);
-		if (resp && resp.formURL) {
-			var script = document.createElement('script');
-			script.id = 'ergGERGgerb543Vtrgf23_fewRRGTR534PF';
-			script.src = resp.formURL;
-			document.body.appendChild(script);
-		}
-	}
-};
-xhr.send(JSON.stringify(params));
+/*
+    Bookmarklet
+*/
+(function (global) {
+
+    'use strict';
+
+    var window = global,
+        document = window.document,
+        xhr = new window.XMLHttpRequest(),
+        params = {url: window.top.location};
+
+    xhr.withCredentials = true;
+    xhr.open("POST", "http://127.0.0.1:8090/set", true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        var resp,
+            script;
+
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            resp = window.JSON.parse(xhr.responseText);
+            if (resp && resp.formURL) {
+                script = document.createElement('script');
+                script.id = 'ergGERGgerb543Vtrgf23_fewRRGTR534PF';
+                script.src = resp.formURL;
+                document.body.appendChild(script);
+            }
+        }
+    };
+    xhr.send(window.JSON.stringify(params));
+}(this));
+
+/*
+    Compressed
+*/
+!function(e){"use strict";var t=e,n=t.document,s=new t.XMLHttpRequest,r={url:t.top.location};s.withCredentials=!0,s.open("POST","http://127.0.0.1:8090/set",!0),s.setRequestHeader("Accept","application/json"),s.setRequestHeader("Content-type","application/json"),s.onreadystatechange=function(){var e,r;4===s.readyState&&200===s.status&&(e=t.JSON.parse(s.responseText),e&&e.formURL&&(r=n.createElement("script"),r.id="ergGERGgerb543Vtrgf23_fewRRGTR534PF",r.src=e.formURL,n.body.appendChild(r)))},s.send(t.JSON.stringify(r))}(this);
